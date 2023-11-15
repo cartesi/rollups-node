ab567b1dab5899a79c1fe221987667bd16252bf8
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-11-14 21:37:23 -0300

    feat: add Go bindings for rollups contracts

---
+++ pkg/contracts/main.go
@@ -0,0 +1,10 @@
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
+// This package contains the Go bindings for the Cartesi Rollups contracts.
+// These bindings are generated automatically by the `generate` binary.
+// To regenerate the bindings, run `go generate` from this directory.
+// To modify the list of contracts, edit the `generate` binary.
+package contracts
+
+//go:generate go run ./generate
