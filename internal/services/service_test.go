ed3ecf483a1e6626c8276cbdec2ee14e06983ac0
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-11-18 17:37:59 -0300

    test: remove Rust binary dependency

--- internal/services/service_test.go
+++ internal/services/service_test.go
@@ -6,6 +6,7 @@ package services
 import (
 	"context"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 	"time"
@@ -13,18 +14,30 @@ import (
 	"github.com/cartesi/rollups-node/internal/logger"
 )
 
+const (
+	servicePort   = "44444"
+	serviceAdress = "0.0.0.0:" + servicePort
+)
+
+var tmpDir string
+
 func setup() {
 	logger.Init("warning", false)
-	setRustBinariesPath()
+	buildFakeService()
+}
+
+func tearDown() {
+	deleteTempDir()
 }
 
 func TestService(t *testing.T) {
+	setup()
 
 	t.Run("it stops when the context is cancelled", func(t *testing.T) {
-		setup()
 		service := Service{
-			name:       "graphql-server",
-			binaryName: "cartesi-rollups-graphql-server",
+			name:            "fake-service",
+			binaryName:      "fake-service",
+			healthcheckPort: servicePort,
 		}
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
@@ -35,18 +48,96 @@ func TestService(t *testing.T) {
 			result <- service.Start(ctx)
 		}()
 
-		// wait for a while
 		time.Sleep(100 * time.Millisecond)
 
-		//shutdown
+		// shutdown
 		cancel()
 		if err := <-result; err != nil {
 			t.Errorf("service exited for the wrong reason: %v", err)
 		}
 	})
+
+	t.Run("it stops when timeout is reached and it isn't ready yet", func(t *testing.T) {
+		service := Service{
+			name:            "fake-service",
+			binaryName:      "fake-service",
+			healthcheckPort: "0000", // wrong port
+		}
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		// start service in goroutine
+		result := make(chan error, 1)
+		go func() {
+			result <- service.Start(ctx)
+		}()
+
+		// expect timeout because of wrong port
+		if err := service.Ready(ctx, 500*time.Millisecond); err == nil {
+			t.Errorf("expected service to timeout")
+		}
+
+		// shutdown
+		cancel()
+		if err := <-result; err != nil {
+			t.Errorf("service exited for the wrong reason: %v", err)
+		}
+	})
+
+	t.Run("it becomes ready soon after being started", func(t *testing.T) {
+		service := Service{
+			name:            "fake-service",
+			binaryName:      "fake-service",
+			healthcheckPort: servicePort,
+		}
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		// start service in goroutine
+		result := make(chan error)
+		go func() {
+			result <- service.Start(ctx)
+		}()
+
+		// wait for service to be ready
+		if err := service.Ready(ctx, 500*time.Millisecond); err != nil {
+			t.Errorf("service timed out")
+		}
+
+		// shutdown
+		cancel()
+		if err := <-result; err != nil {
+			t.Errorf("service exited for the wrong reason: %v", err)
+		}
+	})
+
+	tearDown()
 }
 
-func setRustBinariesPath() {
-	rustBinPath, _ := filepath.Abs("../../offchain/target/debug")
-	os.Setenv("PATH", os.Getenv("PATH")+":"+rustBinPath)
+// Builds the fake-service binary and adds it to PATH
+func buildFakeService() {
+	temporaryDir, err := os.MkdirTemp("", "")
+	if err != nil {
+		panic(err)
+	}
+	tmpDir = temporaryDir
+
+	cmd := exec.Command(
+		"go",
+		"build",
+		"-o",
+		filepath.Join(tmpDir, "fake-service"),
+		"fakeservice/main.go",
+	)
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+	os.Setenv("PATH", os.Getenv("PATH")+":"+tmpDir)
+}
+
+func deleteTempDir() {
+	err := os.RemoveAll(tmpDir)
+	if err != nil {
+		panic(err)
+	}
 }
