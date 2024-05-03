102470d780a19447914e7aa1a59b991bcc856c39
Gustavo Madeira Krieger <gustavo.m.krieger@gmail.com>
2024-05-03 14:26:14 -0300

    feat(graphql): add postraphile to supervisor

--- internal/node/handlers.go
+++ internal/node/handlers.go
@@ -4,8 +4,11 @@
 package node
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 
 	"github.com/cartesi/rollups-node/internal/node/config"
 )
@@ -13,10 +16,25 @@ import (
 func newHttpServiceHandler(c config.NodeConfig) http.Handler {
 	handler := http.NewServeMux()
 	handler.Handle("/healthz", http.HandlerFunc(healthcheckHandler))
+
+	graphqlProxy := newReverseProxy(c.HttpAddress, getPort(c, portOffsetPostgraphile))
+	handler.Handle("/graphql", graphqlProxy)
+	handler.Handle("/graphiql", graphqlProxy)
+
 	return handler
 }
 
 func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	slog.Debug("Node received a healthcheck request")
 	w.WriteHeader(http.StatusOK)
+}
+
+func newReverseProxy(address string, port int) *httputil.ReverseProxy {
+	urlStr := fmt.Sprintf("http://%v:%v/", address, port)
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse url: %v", err))
+	}
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	return proxy
 }
