02305f0ce4594a52609f0810e30fadb0d539bfd9
Francisco Moura <franciscoeduardomoura@gmail.com>
2024-07-05 10:35:28 -0300

    feat(input-reader): Add debug logs

--- internal/util/retry.go
+++ internal/util/retry.go
@@ -3,7 +3,13 @@
 
 package util
 
-import "time"
+import (
+	"context"
+	"log/slog"
+	"reflect"
+	"runtime"
+	"time"
+)
 
 // Implments a simple method call retry policy.
 // This retry policy will retry a failed execution for up to 'maxRetries' times
@@ -20,14 +26,31 @@ func CallFunctionWithRetryPolicy[
 
 	var lastErr error
 	var lastalue R
+	var funcName string
+	if slog.Default().Enabled(context.Background(), slog.LevelDebug) {
+		funcName = getFunctionName(fn)
+	}
 	for i := uint(0); i <= maxRetries; i++ {
 		lastalue, lastErr = fn(args)
 		if lastErr == nil {
 			return lastalue, nil
 		}
-
+		if slog.Default().Enabled(context.Background(), slog.LevelDebug) {
+			slog.Debug(lastErr.Error())
+			slog.Debug("Retrying '" + funcName + "'")
+		}
 		time.Sleep(maxDelay)
 	}
 	return lastalue, lastErr
 
+}
+
+func getFunctionName(fn interface{}) string {
+	function := runtime.FuncForPC(
+		reflect.ValueOf(fn).Pointer(),
+	)
+	if function != nil {
+		return function.Name()
+	}
+	return "N/A"
 }
