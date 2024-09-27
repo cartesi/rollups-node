1cc3edddfd93eab0c08c6ccf43f9aed61c0f4e27
Victor Fusco <1221933+vfusco@users.noreply.github.com>
2024-09-27 12:41:45 -0300

    refactor: remove postgraphile reverse proxy

--- internal/node/handlers.go
+++ internal/node/handlers.go
@@ -4,11 +4,8 @@
 package node
 
 import (
-	"fmt"
 	"log/slog"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
 
 	"github.com/cartesi/rollups-node/internal/inspect"
 	"github.com/cartesi/rollups-node/internal/node/config"
@@ -17,10 +14,6 @@ import (
 func newHttpServiceHandler(c config.NodeConfig, i *inspect.Inspector) http.Handler {
 	handler := http.NewServeMux()
 	handler.Handle("/healthz", http.HandlerFunc(healthcheckHandler))
-
-	graphqlProxy := newReverseProxy(c.HttpAddress, getPort(c, portOffsetPostgraphile))
-	handler.Handle("/graphql", graphqlProxy)
-	handler.Handle("/graphiql", graphqlProxy)
 
 	handler.Handle("/inspect/{dapp}", http.Handler(i))
 	handler.Handle("/inspect/{dapp}/{payload}", http.Handler(i))
@@ -31,14 +24,4 @@ func newHttpServiceHandler(c config.NodeConfig, i *inspect.Inspector) http.Handl
 func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	slog.Debug("Node received a healthcheck request")
 	w.WriteHeader(http.StatusOK)
-}
-
-func newReverseProxy(address string, port int) *httputil.ReverseProxy {
-	urlStr := fmt.Sprintf("http://%v:%v/", address, port)
-	url, err := url.Parse(urlStr)
-	if err != nil {
-		panic(fmt.Sprintf("failed to parse url: %v", err))
-	}
-	proxy := httputil.NewSingleHostReverseProxy(url)
-	return proxy
 }
